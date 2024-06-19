7dbc78a14c00a6e2397ca67174095500ab27efb3
Nanonimous <COCNIVAAS@GMAIL.COM>
2024-06-19 12:39:09 +0530

    Switch to bcryptjs to avoid native module issues

--- backend_axios.js
+++ backend_axios.js
@@ -4,7 +4,7 @@ import {dirname} from "path";
 import { fileURLToPath } from "url";
 import axios from "axios";
 import multer from "multer";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import session from "express-session";
